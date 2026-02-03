3ea17f90ffe0c9ec2098cce8a2d0688331ae328c
sairanovamir14 <sairanovamir14@gmail.com>
2026-02-03 20:20:48 +0500

    Connect frontend templates

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="../frontend/templates")
+app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
+
 @app.get("/")
-def home():
-    return {"status": "ok"}
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
